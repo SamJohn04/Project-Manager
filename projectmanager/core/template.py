5e2fa71ed13cae525781d5b84f470fa5c69957a3
SamJohn04 <samuel.john.codes@gmail.com>
2024-12-12 21:56:26 +0530

    refactor: placeholder changed for clarity and norm

--- projectmanager/core/template.py
+++ projectmanager/core/template.py
@@ -34,7 +34,7 @@ def create_from_template(project_title: str, template_data: dict, verbosity_leve
 
 
 def create_file(project_title: str, file_path_to_create: Path, content: str):
-    content = content.replace(config.TEMPLATE_TITLE_FLAG, project_title)
+    content = content.replace(config.TITLE_PLACEHOLDER, project_title)
 
     file_path_to_create.parent.mkdir(parents=True, exist_ok=True)
     with open(file_path_to_create, 'w') as file:
