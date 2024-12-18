6bb20b93f516ad2abdb54cbad886e166d61b4844
SamJohn04 <samuel.john.codes@gmail.com>
2024-12-18 21:26:11 +0530

    change[io]: read_specification to error if spec data does not exist or is invalid; feat[io]: specification_exists method to check if spec data is present as a file;

--- projectmanager/util/io.py
+++ projectmanager/util/io.py
@@ -7,22 +7,21 @@ from projectmanager.validation.specification import validate_spec
 from projectmanager.validation.template import validate_template
 
 
-def read_specification() -> dict | None:
+def specification_exists() -> bool:
+    return Path(PM_SPECIFICATION_FILE_NAME).is_file()
+
+
+def read_specification() -> dict:
     if not Path(PM_SPECIFICATION_FILE_NAME).is_file():
-        return None
+        raise Exception("Specification not found. Please initialize the specification first.")
 
-    try:
-        with open(PM_SPECIFICATION_FILE_NAME, encoding='utf-8') as file:
-            json_data = json.load(file)
-    except Exception as e:
-        err(f"Something went wrong when parsing the specification file: {e}")
-        exit(1)
+    with open(PM_SPECIFICATION_FILE_NAME, encoding='utf-8') as file:
+        spec_data = json.load(file)
     
-    if not validate_spec(json_data):
-        err("Your specification is invalid. It is recommended to reinitialize the specification.")
-        exit(1)
+    if not validate_spec(spec_data):
+        raise Exception("Your specification is invalid. It is recommended to reinitialize the specification.")
 
-    return json_data
+    return spec_data
 
 
 def write_specification(specification: dict):
