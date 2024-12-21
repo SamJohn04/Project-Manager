e8b7b3b1ab35ce2bda972c16dbb1a6e2728609c9
SamJohn04 <samuel.john.codes@gmail.com>
2024-12-21 22:10:31 +0530

    refactor[feat/status]: replace scan_command method with feature/status implementation;

--- projectmanager/feature/status.py
+++ projectmanager/feature/status.py
@@ -1,5 +1,22 @@
-# @FEAT status TODO
-def status():
-    # TODO
-    pass
+from argparse import Namespace
+from projectmanager import config
+from projectmanager.core import scan
+from projectmanager.util import io, style
+
+
+# @FEAT status ON-HOLD refactor core.scan necessary;
+def status(args: Namespace):
+    spec_data = io.read_specification()
+    if len(spec_data.get("pathGroups", [])) == 0:
+        if args.verbosity != config.V_QUIET:
+            io.warn("No path groups found. Please add a path group to scan.")
+        return
+
+    for path_group in spec_data["pathGroups"]:
+        print(style.bold(f"Path Group: {path_group['name']}"))
+        print(f"Scanning {path_group['name']} for todos...")
+        scan.scan_path_group_for_todos(path_group, spec_data.get("options", {}).get("todoFlag"), args.verbosity)
+        print(f"\nScanning {path_group['name']} for objectives...")
+        scan.scan_path_group_for_objectives(path_group, spec_data.get("objectives", []), spec_data.get("options", {}).get("objectiveFlag"), args.verbosity)
+        print()
 
--- projectmanager/main.py
+++ projectmanager/main.py
@@ -6,6 +6,7 @@ from projectmanager.feature.add_item import add_item
 from projectmanager.feature.init import init
 from projectmanager.feature.remove import remove
 from projectmanager.feature.set_options import set_options
+from projectmanager.feature.status import status
 from projectmanager.feature.view import view
 from projectmanager.util import io, style
 
@@ -23,7 +24,7 @@ def main():
         case "rm":
             remove(args)
         case "status" | "scan":
-            scan_command(args.verbosity)
+            status(args)
         case "generate":
             generate(args.objective, args.path_group, args.verbosity)
         case "set" | "unset":
@@ -40,24 +41,6 @@ def generate(objective_name: str | None, path_group: str | None, verbosity_level
         generate_objective_content(spec_data, objective["name"], path_group)
         if verbosity_level != config.V_QUIET:
             io.success(f"Code of objective {objective['name']} has been generated successfully.")
-
-
-# @FEAT scan DONE
-def scan_command(verbosity_level: int = config.V_NORMAL):
-    spec_data = get_spec_data()
-
-    if len(spec_data.get("pathGroups", [])) == 0:
-        if verbosity_level != config.V_QUIET:
-            io.warn("No path groups found. Please add a path group to scan.")
-        exit(1)
-
-    for path_group in spec_data["pathGroups"]:
-        print(style.bold(f"Path Group: {path_group['name']}"))
-        print(f"Scanning {path_group['name']} for todos...")
-        scan.scan_path_group_for_todos(path_group, spec_data.get("options", {}).get("todoFlag"), verbosity_level)
-        print(f"\nScanning {path_group['name']} for objectives...")
-        scan.scan_path_group_for_objectives(path_group, spec_data.get("objectives", []), spec_data.get("options", {}).get("objectiveFlag"), verbosity_level)
-        print()
 
 
 def get_spec_data() -> dict:
