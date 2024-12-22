a50c8e8467eebf2e7407fffbf3c6907548038f4d
SamJohn04 <samuel.john.codes@gmail.com>
2024-12-22 16:51:03 +0530

    fix: exceptions caught and displayed before exit;

--- projectmanager/main.py
+++ projectmanager/main.py
@@ -1,3 +1,4 @@
+from argparse import Namespace
 from projectmanager.arg_parse import parse_args
 
 from projectmanager.feature.add_item import add_item
@@ -8,10 +9,19 @@ from projectmanager.feature.set_options import set_options
 from projectmanager.feature.status import status
 from projectmanager.feature.view import view
 
+from projectmanager.util import io
+
 
 def main():
     args = parse_args()
 
+    try:
+        act(args)
+    except Exception as e:
+        io.err(str(e))
+
+
+def act(args: Namespace):
     match args.command:
         case "init":
             init(args)
