3319e514922097517d2ee874cb37fe8d0c9e9fe2
SamJohn04 <samuel.john.codes@gmail.com>
2024-12-22 16:41:58 +0530

    chore: remove unused imports and method;

--- projectmanager/main.py
+++ projectmanager/main.py
@@ -1,6 +1,5 @@
-from projectmanager import config
 from projectmanager.arg_parse import parse_args
-from projectmanager.core.generate import generate_objective_content
+
 from projectmanager.feature.add_item import add_item
 from projectmanager.feature.generate import generate
 from projectmanager.feature.init import init
@@ -8,7 +7,6 @@ from projectmanager.feature.remove import remove
 from projectmanager.feature.set_options import set_options
 from projectmanager.feature.status import status
 from projectmanager.feature.view import view
-from projectmanager.util import io
 
 
 def main():
@@ -29,14 +27,6 @@ def main():
             generate(args)
         case "set" | "unset":
             set_options(args)
-
-
-def get_spec_data() -> dict:
-    spec_data = io.read_specification()
-    if spec_data is None:
-        io.err("Specification not found. Please initialize the specification first.")
-        exit(1)
-    return spec_data
 
 
 if __name__ == '__main__':
