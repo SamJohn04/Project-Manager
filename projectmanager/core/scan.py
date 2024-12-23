aee754463cd2bd950f258f804a25e53077caf01b
SamJohn04 <samuel.john.codes@gmail.com>
2024-12-23 22:56:13 +0530

    refactor[scan]: scanning and display of results decoupled

--- projectmanager/core/scan.py
+++ projectmanager/core/scan.py
@@ -1,122 +1,26 @@
-from projectmanager import config
-from projectmanager.util import io, paths, style
+from collections import namedtuple
+from pathlib import Path
+
+from projectmanager.util import paths
+
+ScanInstance = namedtuple("ScanInstance", ["line", "index", "path", "all_lines"])
 
 
-def scan_path_group_for_todos(path_group: dict, flag: str | None = None, verbosity_level: int = config.V_NORMAL):
-    todo_found_in_group = False
-    if flag is None:
-        flag = config.DEFAULT_TODO_FLAG
-
+def scan_path_group(path_group: dict, flag: str) -> list[ScanInstance]:
+    scan_instances = []
     for path in paths.from_path_group(path_group):
-        with open(path, encoding="utf-8") as file:
-            lines = file.read().splitlines()
-        todo_indices = scan_todos_from_content(lines, flag)
-        display_todos_by_content(path, lines, todo_indices, verbosity_level)
-        if len(todo_indices) > 0:
-            todo_found_in_group = True
-
-    if not todo_found_in_group and verbosity_level != config.V_QUIET:
-        io.success(f"No TODOs found in {path_group['name']}")
+        scan_instances.extend(scan_path(path, flag))
+    return scan_instances
 
 
-def scan_path_group_for_objectives(path_group: dict, objectives: list[dict], flag: str | None = None, verbosity_level: int = config.V_NORMAL):
-    if flag is None:
-        flag = config.DEFAULT_OBJECTIVE_FLAG
+def scan_path(path: Path, flag: str) -> list[ScanInstance]:
+    scan_instances = []
 
-    objective_flags = {objective["name"]: [] for objective in objectives}
-    statuses = {}
-
-    for path in paths.from_path_group(path_group):
-        with open(path, encoding="utf-8") as file:
-            lines = file.read().splitlines()
-        scan_objectives_from_content(lines, flag, str(path), objective_flags, statuses)
-
-    if verbosity_level == config.V_QUIET:
-        total_statuses = sum(statuses.values())
-        for status in statuses:
-            print(f"{style.green_text(status)}: {statuses[status]}/{total_statuses}")
-        return
-
-    if verbosity_level == config.V_NORMAL:
-        for objective in objective_flags:
-            print(objective, end=": ")
-            print(", ".join(status for status, *_ in objective_flags[objective]))
-        print()
-        total_statuses = sum(statuses.values())
-        for status in statuses:
-            print(f"{style.green_text(status)}: {statuses[status]}/{total_statuses}")
-        return
-
-    for objective in objective_flags:
-        if len(objective_flags[objective]) == 0:
-            print("No flag for", objective, "was found in", path_group["name"])
-        if len(objective_flags[objective]) > 0:
-            print(objective)
-            for status, file_name, index in objective_flags[objective]:
-                print(f"\t{status} ({file_name}: line {index + 1})")
-    
-    print()
-    total_statuses = sum(statuses.values())
-    for status in statuses:
-        print(f"{style.green_text(status)}: {statuses[status]}/{total_statuses}")
-
-
-def scan_todos_from_content(lines: list[str], flag: str):
-    todo_indices = []
+    with open(path, encoding="utf-8") as file:
+        lines = file.read().splitlines()
 
     for index, line in enumerate(lines):
         if flag in line:
-            todo_indices.append(index)
-
-    return todo_indices
-
-
-def scan_objectives_from_content(lines: list[str], flag: str, file_name: str, objective_flags: dict, statuses: dict) -> dict[str, int]:
-    for index, line in enumerate(lines):
-        if flag not in line:
-            continue
-        line = line[line.index(flag) + len(flag):].strip()
-        if len(line) == 0:
-            io.warn(f"Unlabeled Objective Flag: {file_name} (line {index + 1})")
-            continue
-
-        parts = line.split()
-
-        objective = parts[0]
-        status = "unmarked" if len(parts) == 1 or len(parts[1]) == 0 else parts[1]
-
-        if objective not in objective_flags:
-            io.warn(f"Unrecognized Objective \"{objective}\" (status: {status}) in {file_name} (line {index + 1})")
-        else:
-            objective_flags[objective].append((status, file_name, index))
-        
-        if status not in statuses:
-            statuses[status] = 0
-        statuses[status] += 1
-
-    return statuses
-
-
-def display_todos_by_content(file_name, lines: list[str], todo_indices: list[int], verbosity_level: int = config.V_NORMAL):
-    if len(todo_indices) == 0:
-        return
-
-    if verbosity_level == config.V_QUIET:
-        print(file_name, end=": ")
-        print(', '.join(str(todo_index) for todo_index in todo_indices))
-        return
-
-    if verbosity_level == config.V_NORMAL:
-        print(file_name)
-        for todo_index in todo_indices:
-            print(style.bold(f"\t{todo_index + 1}. {lines[todo_index]}"))
-        return
-
-    print(file_name)
-    for todo_index in todo_indices:
-        if todo_index > 0:
-            print(f"\t{todo_index}. {lines[todo_index-1]}")
-        print(style.bold(f"\t{todo_index + 1}. {lines[todo_index]}"))
-        if todo_index < len(lines) - 1:
-            print(f"\t{todo_index + 2}. {lines[todo_index+1]}")
+            scan_instances.append(ScanInstance(line, index, path, lines))
+    return scan_instances
 
--- projectmanager/feature/status.py
+++ projectmanager/feature/status.py
@@ -5,7 +5,7 @@ from projectmanager.core import scan
 from projectmanager.util import io, style
 
 
-# @FEAT status ON-HOLD refactor core.scan necessary;
+# @FEAT status REVIEW
 def status(args: Namespace):
     spec_data = io.read_specification()
     if len(spec_data.get("pathGroups", [])) == 0:
@@ -13,11 +13,90 @@ def status(args: Namespace):
             io.warn("No path groups found. Please add a path group to scan.")
         return
 
+    todo_flag = spec_data.get("options", {}).get("todoFlag", config.DEFAULT_TODO_FLAG)
+    objective_flag = spec_data.get("options", {}).get("objectiveFlag", config.DEFAULT_OBJECTIVE_FLAG)
+
+    objective_names = {objective["name"] for objective in spec_data.get("objectives", [])}
+
     for path_group in spec_data["pathGroups"]:
-        print(style.bold(f"Path Group: {path_group['name']}"))
-        print(f"Scanning {path_group['name']} for todos...")
-        scan.scan_path_group_for_todos(path_group, spec_data.get("options", {}).get("todoFlag"), args.verbosity)
-        print(f"\nScanning {path_group['name']} for objectives...")
-        scan.scan_path_group_for_objectives(path_group, spec_data.get("objectives", []), spec_data.get("options", {}).get("objectiveFlag"), args.verbosity)
+        print(style.bold(f"Path Group {path_group['name']}: TODOs"))
+        todo_instances = scan.scan_path_group(path_group, todo_flag)
+        display_todo_instances(todo_instances, args.verbosity)
         print()
+
+        print(style.bold(f"Path Group {path_group['name']}: Objectives"))
+        objective_instances = scan.scan_path_group(path_group, objective_flag)
+        display_objective_instances(objective_instances, objective_names, objective_flag, args.verbosity)
+        print()
+
+
+def display_todo_instances(scan_instances: list[scan.ScanInstance], verbosity_level: int = config.V_NORMAL):
+    if len(scan_instances) == 0:
+        print("No TODOs found!")
+        return
+
+    todo_instances_by_path = {}
+
+    for scan_instance in scan_instances:
+        if scan_instance.path not in todo_instances_by_path:
+            todo_instances_by_path[scan_instance.path] = []
+        todo_instances_by_path[scan_instance.path].append(scan_instance)
+
+    for path in todo_instances_by_path:
+        if verbosity_level == config.V_QUIET:
+            print(f"{path}:", ', '.join(todo_instance.index for todo_instance in todo_instances_by_path[path]))
+            continue
+        print(path)
+        if verbosity_level == config.V_NORMAL:
+            for todo_instance in todo_instances_by_path[path]:
+                print(f"\t{todo_instance.index + 1}. {todo_instance.line}")
+            continue
+        for todo_instance in todo_instances_by_path[path]:
+            if todo_instance.index > 0:
+                print(f"\t{todo_instance.index}. {todo_instance.all_lines[todo_instance.index - 1]}")
+            print(style.bold(f"\t{todo_instance.index + 1}. {todo_instance.line}"))
+            if todo_instance.index < len(todo_instance.all_lines) - 1:
+                print(f"\t{todo_instance.index + 2}. {todo_instance.all_lines[todo_instance.index + 1]}")
+
+
+def display_objective_instances(scan_instances: list[scan.ScanInstance], objective_names: set[str], objective_flag: str, verbosity_level: int = config.V_NORMAL):
+    found_objectives = {objective_name: [] for objective_name in objective_names}
+    statuses = {}
+
+    for scan_instance in scan_instances:
+        line_items = scan_instance.line[scan_instance.line.index(objective_flag) + len(objective_flag):].strip().split()
+
+        if len(line_items) == 0:
+            io.warn(f"Unlabeled Objective Flag: {scan_instance.path} (line {scan_instance.index + 1})")
+            continue
+
+        objective = line_items[0]
+        status = line_items[1] if len(line_items) > 1 else "Unmarked"
+
+        if objective in objective_names:
+            found_objectives[objective].append((status, scan_instance.path, scan_instance.index))
+        else:
+            io.warn(f"Unrecognized Objective \"{objective}\" (status: {status}) in {scan_instance.path} (line {scan_instance.index + 1})")
+
+        if status not in statuses:
+            statuses[status] = 0
+        statuses[status] += 1
+
+    if verbosity_level == config.V_NORMAL:
+        for objective in found_objectives:
+            print(f"{objective}:", ", ".join(status for status, *_ in found_objectives[objective]))
+        print()
+    elif verbosity_level == config.V_VERBOSE:
+        for objective in found_objectives:
+            if len(found_objectives[objective]) == 0:
+                print(f"No flag for {objective} was found.")
+                continue
+            print(objective)
+            for status, file_name, index in found_objectives[objective]:
+                print(f"\t{status} ({file_name}: line {index + 1})")
+        print()
+
+    total_statuses = sum(statuses.values())
+    for status in statuses:
+        print(f"{style.green_text(status)}: {statuses[status]}/{total_statuses}")
 
