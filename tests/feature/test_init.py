6ab613affa4621efa536d26d6c9e5559708f1da1
SamJohn04 <samuel.john.codes@gmail.com>
2024-12-26 20:48:18 +0530

    test: init test for template init;

--- tests/feature/test_init.py
+++ tests/feature/test_init.py
@@ -8,9 +8,10 @@ from projectmanager.feature import init
 from projectmanager import config
 
 
-# @FEAT init IN-PROGRESS
+# @FEAT init REVIEW
 def test_init():
     parser = arg_parse.get_parser()
+
     with tempfile.TemporaryDirectory() as temp_dir_name:
         os.chdir(temp_dir_name)
 
@@ -23,6 +24,27 @@ def test_init():
             json_data = json.load(spec_file)
 
         assert json_data == {'title': 'title'}
+
+    template_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'templates', 'pytemplate.json')
+
+    with tempfile.TemporaryDirectory() as temp_dir_name:
+        os.chdir(temp_dir_name)
+
+        init.init(parser.parse_args(['init', '--template', template_path, 'title']))
+
+        spec_file_path = pathlib.Path(config.PM_SPECIFICATION_FILE_NAME)
+        assert spec_file_path.is_file()
+
+        with open(spec_file_path, encoding='utf-8') as spec_file:
+            json_data = json.load(spec_file)
+
+        assert json_data['title'] == 'title'
+        assert len(json_data['pathGroups']) == 2
+        assert json_data['pathGroups'][0]['name'] == 'main' and json_data['pathGroups'][0]['dirPath'] == 'src'
+        assert json_data['pathGroups'][1]['name'] == 'test' and json_data['pathGroups'][1]['dirPath'] == 'tests'
+
+        assert (pathlib.Path() / 'src' / 'main.py').is_file()
+        assert (pathlib.Path() / 'tests').is_dir()
 
 
 def test_get_items_to_create():
